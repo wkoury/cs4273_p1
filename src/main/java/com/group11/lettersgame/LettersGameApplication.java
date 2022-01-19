d5b92783700718f5bfe3c879d147608b6d21429f
Will Koury <wkoury@hey.com>
2022-01-19 11:17:19 -0600

    Re-bootstrap the project using Gradle spring boot generator

---
+++ src/main/java/com/group11/lettersgame/LettersGameApplication.java
@@ -0,0 +1,13 @@
+package com.group11.lettersgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettersGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettersGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group11/lettersgame/LettersGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group11.lettersgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettersGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

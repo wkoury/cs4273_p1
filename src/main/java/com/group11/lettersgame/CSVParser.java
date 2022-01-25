6ca823e1a559f90b7448894dae8bbb6592ec1565
HunterDarr <hunter.darr.one@gmail.com>
2022-01-25 14:32:10 -0600

    Made changes to CSVParser. Added getLetters() Method. Changed Letter class to String instead of char.

--- src/main/java/com/group11/lettersgame/CSVParser.java
+++ src/main/java/com/group11/lettersgame/CSVParser.java
@@ -1,5 +1,39 @@
+
 package com.group11.lettersgame;
+import java.io.*;
+import java.util.ArrayList;
 
 public class CSVParser {
+
+    String fileName;
+    ArrayList<Letter> letters;
+
+    CSVParser(String fileName) {
+        this.fileName = fileName;
+        getLetters();
+    }
+
+    private void getLetters() {
+        
+        String addressName = "C:/src/main/data/" + fileName;
+        try {
+            File file = new File(addressName);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            String[] temp;
+            while ((line = bufferedReader.readLine()) != null) {
+                temp = line.split(",");
+                    Letter newLetter = new Letter(temp[0], temp[1]);
+                    letters.add(newLetter);
+                
+            }
+            bufferedReader.close();
+        } catch(Exception e) {
+
+        }
+    }
+
+
 
 }
--- src/main/java/com/group11/lettersgame/Letter.java
+++ src/main/java/com/group11/lettersgame/Letter.java
@@ -1,19 +1,19 @@
 package com.group11.lettersgame;
 
 public class Letter {
-	private char lowercase;
-	private char uppercase;
+	private String lowercase;
+	private String uppercase;
 
-	public Letter(char l, char u) {
+	public Letter(String l, String u) {
 		this.lowercase = l;
 		this.uppercase = u;
 	}
 
-	public char getLowercase() {
+	public String getLowercase() {
 		return this.lowercase;
 	}
 
-	public char getUppercase() {
+	public String getUppercase() {
 		return this.uppercase;
 	}
 }

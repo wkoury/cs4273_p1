1c834152b5cc8a21c476630fba9db2a86bd4946f
Will Koury <wkoury@hey.com>
2022-01-25 13:36:42 -0600

    Add routes for the rest of the languages

--- src/main/java/com/group11/lettersgame/Controller.java
+++ src/main/java/com/group11/lettersgame/Controller.java
@@ -20,7 +20,53 @@ public class Controller {
 	public ResponseEntity<HttpStatus> getEnglish() {
 		// 1. Read the English CSV file
 		// 2. Send the random number and the corresponding letter in JSON format
-		//
+
+		System.out.println("This is an HTTP request to /api/english");
+
+		// This is filler code to make the compiler happy temporarily
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/api/spanish")
+	public ResponseEntity<HttpStatus> getSpanish() {
+				// 1. Read the Spanish CSV file
+		// 2. Send the random number and the corresponding letter in JSON format
+
+		System.out.println("This is an HTTP request to /api/spanish");
+
+		// This is filler code to make the compiler happy temporarily
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/api/igbo")
+	public ResponseEntity<HttpStatus> getIgbo() {
+				// 1. Read the Igbo CSV file
+		// 2. Send the random number and the corresponding letter in JSON format
+
+		System.out.println("This is an HTTP request to /api/igbo");
+
+		// This is filler code to make the compiler happy temporarily
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/api/yoruba")
+	public ResponseEntity<HttpStatus> getYoruba() {
+				// 1. Read the Yoruba CSV file
+		// 2. Send the random number and the corresponding letter in JSON format
+
+		System.out.println("This is an HTTP request to /api/yoruba");
+
+		// This is filler code to make the compiler happy temporarily
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/api/hausa")
+	public ResponseEntity<HttpStatus> getHausa() {
+		// 1. Read the Hausa CSV file
+		// 2. Send the random number and the corresponding letter in JSON format
+
+		System.out.println("This is an HTTP request to /api/hausa");
+
 		// This is filler code to make the compiler happy temporarily
 		return new ResponseEntity<>(HttpStatus.OK);
 	}

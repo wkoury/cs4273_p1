a0758fa8ceb5ce2f252feca30942d2812f4ea36d
Will Koury <wkoury@hey.com>
2022-01-26 17:49:41 -0600

    Add some example unit tests

--- src/test/java/com/group11/lettersgame/LettersGameApplicationTests.java
+++ src/test/java/com/group11/lettersgame/LettersGameApplicationTests.java
@@ -1,13 +1,24 @@
 package com.group11.lettersgame;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LettersGameApplicationTests {
 
+	// this was just me refreshing my knowledge of writing unit tests in java
 	@Test
-	void contextLoads() {
+	void twoPlusTwoEqualsFour() {
+		assertEquals(2 + 2, 4);
+	}
+
+	@Test
+	void testLetterGetLowercase() {
+		Letter a = new Letter("a", "A");
+
+		assertEquals(a.getLowercase(), "a");
+		assertEquals(a.getUppercase(), "A");
 	}
 
 }

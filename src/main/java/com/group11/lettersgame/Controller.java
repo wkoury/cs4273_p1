d0e2620b88cb37bf582917a4136f558a96ae3b5b
Will Koury <wkoury@hey.com>
2022-01-26 17:48:21 -0600

    Implement the API

--- src/main/java/com/group11/lettersgame/Controller.java
+++ src/main/java/com/group11/lettersgame/Controller.java
@@ -1,5 +1,7 @@
 package com.group11.lettersgame;
 
+import java.util.Map;
+import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.apache.coyote.Response;
@@ -11,63 +13,93 @@ import java.security.Principal;
 @RestController
 @RequestMapping("/")
 public class Controller {
-	@GetMapping("/test")
+	@GetMapping("/api/health")
 	public ResponseEntity<HttpStatus> get() {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@GetMapping("/api/english")
-	public ResponseEntity<HttpStatus> getEnglish() {
+	public Map<String, String> getEnglish() {
 		// 1. Read the English CSV file
 		// 2. Send the random number and the corresponding letter in JSON format
 
 		System.out.println("This is an HTTP request to /api/english");
+		CSVParser english = new CSVParser("english.csv");
 
-		// This is filler code to make the compiler happy temporarily
-		return new ResponseEntity<>(HttpStatus.OK);
+		Letter l = english.getRandomLetter();
+
+		HashMap<String, String> ret = new HashMap<String, String>();
+		ret.put("letter", l.getLowercase());
+		ret.put("number", Integer.toString(l.getIndexPlusOne()));
+
+		return ret;
 	}
 
 	@GetMapping("/api/spanish")
-	public ResponseEntity<HttpStatus> getSpanish() {
+	public Map<String, String> getSpanish() {
 				// 1. Read the Spanish CSV file
 		// 2. Send the random number and the corresponding letter in JSON format
 
 		System.out.println("This is an HTTP request to /api/spanish");
+		CSVParser spanish = new CSVParser("spanish.csv");
 
-		// This is filler code to make the compiler happy temporarily
-		return new ResponseEntity<>(HttpStatus.OK);
+		Letter l = spanish.getRandomLetter();
+
+		HashMap<String, String> ret = new HashMap<String, String>();
+		ret.put("letter", l.getLowercase());
+		ret.put("number", Integer.toString(l.getIndexPlusOne()));
+
+		return ret;
 	}
 
 	@GetMapping("/api/igbo")
-	public ResponseEntity<HttpStatus> getIgbo() {
+	public Map<String, String> getIgbo() {
 				// 1. Read the Igbo CSV file
 		// 2. Send the random number and the corresponding letter in JSON format
 
 		System.out.println("This is an HTTP request to /api/igbo");
+		CSVParser igbo = new CSVParser("igbo.csv");
 
-		// This is filler code to make the compiler happy temporarily
-		return new ResponseEntity<>(HttpStatus.OK);
+		Letter l = igbo.getRandomLetter();
+
+		HashMap<String, String> ret = new HashMap<String, String>();
+		ret.put("letter", l.getLowercase());
+		ret.put("number", Integer.toString(l.getIndexPlusOne()));
+
+		return ret;
 	}
 
 	@GetMapping("/api/yoruba")
-	public ResponseEntity<HttpStatus> getYoruba() {
+	public Map<String, String> getYoruba() {
 				// 1. Read the Yoruba CSV file
 		// 2. Send the random number and the corresponding letter in JSON format
 
 		System.out.println("This is an HTTP request to /api/yoruba");
+		CSVParser yoruba = new CSVParser("yoruba.csv");
 
-		// This is filler code to make the compiler happy temporarily
-		return new ResponseEntity<>(HttpStatus.OK);
+		Letter l = yoruba.getRandomLetter();
+
+		HashMap<String, String> ret = new HashMap<String, String>();
+		ret.put("letter", l.getLowercase());
+		ret.put("number", Integer.toString(l.getIndexPlusOne()));
+
+		return ret;
 	}
 
 	@GetMapping("/api/hausa")
-	public ResponseEntity<HttpStatus> getHausa() {
+	public Map<String, String> getHausa() {
 		// 1. Read the Hausa CSV file
 		// 2. Send the random number and the corresponding letter in JSON format
 
 		System.out.println("This is an HTTP request to /api/hausa");
+		CSVParser hausa = new CSVParser("hausa.csv");
 
-		// This is filler code to make the compiler happy temporarily
-		return new ResponseEntity<>(HttpStatus.OK);
+		Letter l = hausa.getRandomLetter();
+
+		HashMap<String, String> ret = new HashMap<String, String>();
+		ret.put("letter", l.getLowercase());
+		ret.put("number", Integer.toString(l.getIndexPlusOne()));
+
+		return ret;
 	}
 }
